93f5cda8935bedd56bbfdb49d1f556ac714493a1
gosurvashi29 <urvashigoswami350@gmail.com>
2025-03-03 19:09:30 +0530

    added database file and db connection

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+const path=require("path")
+
+const getProducts=(req, res) => {
+  
+    res.sendFile(path.join(__dirname,"../views/product.html"));
+  };
+
+  const getProductForm=(req, res) => {
+  
+    res.sendFile(path.join(__dirname,"../views/form.html"));
+  };
+
+
+
+  const getProductsById=(req, res) => {
+  
+    res.send(`Fetching product with ID: ${req.params.id}`);
+  };
+
+
+
+  const postProducts=(req, res) => {
+    const data=req.body;
+    res.json({value:data.productName})
+    //res.send("Adding a new product");
+   };
+
+   const putProducts=(req, res) => {
+    res.send("Put request called.");
+   };
+   
+
+   const deleteProducts=(req, res) => {
+    res.send("Delete request called.");
+   };
+
+  module.exports={
+    getProducts,
+    getProductForm,
+    getProductsById,
+    postProducts,putProducts,
+    deleteProducts
+  }
---
+++ routes/products.js
@@ -0,0 +1,20 @@
+const express= require('express');
+
+const router=express.Router();
+
+const productController=require("../controllers/productController")
+
+
+router.get('/', productController.getProducts);
+
+router.get('/form', productController.getProductForm);
+
+router.post('/', productController.postProducts);
+
+router.get('/:id', productController.getProductsById);
+
+router.put('/:id', productController.putProducts);
+
+router.delete('/:id', productController.deleteProducts);
+
+module.exports=router;
